95384dc8c0fb97d4f21ab10ab6882aef607aa750
latfulinda <latfulinda@ya.ru>
2021-12-06 17:26:01 +0300

    lesson 4_2 step 6

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout) #команду для неявного ожидания
+
+    def open(self):
+	    self.browser.get(self.url)
+	
+    def is_element_present(self, how, what): #перехватывает исключение
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
